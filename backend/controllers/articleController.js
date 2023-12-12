ad679bff0c36769e311538ecc3a22ed6808b985e
EliasSahlos <esahlos12@gmail.com>
2023-12-12 18:22:56 +0200

    Connection to Firebase DB completed && added logic to convert Date field from String to Timestamp in db.

--- backend/controllers/articleController.js
+++ backend/controllers/articleController.js
@@ -1,22 +1,43 @@
+const {collection, addDoc, Timestamp} = require('firebase/firestore');
 const {fetchArticlesFromWebsite} = require('../scrapper')
+const {db} = require('../firebase/firebase')
 const websites = require('../websites')
 
 async function getAllArticles(req, res) {
-    const categories = ['economy', 'politics'];
-    const allArticles = {
-        economyArticles: [],
-        politicsArticles: []
-    };
-    for (const website in websites) {
-        for (const category of categories) {
-            const articles = await fetchArticlesFromWebsite(website, category);
-            //Inserts new articles to specific category without losing previous articles
-            allArticles[`${category}Articles`] = allArticles[`${category}Articles`].concat(articles);
-        }
-    }
+    try {
+        const categories = ['economy', 'politics'];
+        const allArticles = {
+            economyArticles: [],
+            politicsArticles: []
+        };
 
-    res.json(allArticles);
+        for (const website in websites) {
+            for (const category of categories) {
+                const articles = await fetchArticlesFromWebsite(website, category);
+                allArticles[`${category}Articles`] = allArticles[`${category}Articles`].concat(articles);
+
+                // Convert date strings to Timestamps
+                const articlesWithTimestamps = articles.map(article => {
+                    // Assuming 'date' is the key containing the date string
+                    const dateTimestamp = Timestamp.fromDate(new Date(article.date)); // Convert the string to Timestamp
+                    return { ...article, date: dateTimestamp }; // Replace the date string with the Timestamp
+                });
+
+                // Save articles to Firestore
+                const categoryRef = collection(db, 'articles', category,'list'); // Check this line
+
+                for (const article of articlesWithTimestamps) {
+                    await addDoc(categoryRef, article); // Check this line as well
+                }
+            }
+        }
+
+        res.json(allArticles);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 }
+
 
 async function getEconomyArticles(req, res) {
     const categories = ['economy']

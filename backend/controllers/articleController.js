25117c43ee29eaf9e9df75a0744f323a53850d1a
EliasSahlos <esahlos12@gmail.com>
2023-12-13 00:43:29 +0200

    Only new articles upload to Firestore && implemented auto fetching articles every 1 minute

--- backend/controllers/articleController.js
+++ backend/controllers/articleController.js
@@ -1,66 +1,69 @@
-const {collection, addDoc, Timestamp} = require('firebase/firestore');
-const {fetchArticlesFromWebsite} = require('../scrapper')
-const {db} = require('../firebase/firebase')
-const websites = require('../websites')
+const { CronJob } = require('cron');
+const { collection, addDoc, Timestamp, getDocs, query } = require('firebase/firestore');
+const { fetchArticlesFromWebsite } = require('../scrapper');
+const { db } = require('../firebase/firebase');
+const websites = require('../websites');
 
-async function getAllArticles(req, res) {
+async function fetchAndProcessArticles() {
     try {
+        console.log("Starting fetchAndProcessArticles function..");
         const categories = ['economy', 'politics'];
         const allArticles = {
             economyArticles: [],
             politicsArticles: []
         };
+        let newArticlesCounter = 0;
+        let newArticles = [];
 
         for (const website in websites) {
             for (const category of categories) {
+                //Fetch articles from websites
                 const articles = await fetchArticlesFromWebsite(website, category);
                 allArticles[`${category}Articles`] = allArticles[`${category}Articles`].concat(articles);
-
-                // Convert date strings to Timestamps
-                const articlesWithTimestamps = articles.map(article => {
-                    // Assuming 'date' is the key containing the date string
-                    const dateTimestamp = Timestamp.fromDate(new Date(article.date)); // Convert the string to Timestamp
-                    return { ...article, date: dateTimestamp }; // Replace the date string with the Timestamp
+                
+                //Check for existing articles in Firestore base on article field text 
+                const existingArticleArticles = [];
+                const querySnapshot = await getDocs(query(collection(db, 'articles', category, 'list')));
+                querySnapshot.forEach((doc) => {
+                    existingArticleArticles.push(doc.data().article);
                 });
 
-                // Save articles to Firestore
-                const categoryRef = collection(db, 'articles', category,'list'); // Check this line
+                //Filter articles that don't exist in Firestore
+                const newArticlesFiltered = articles.filter(article => !existingArticleArticles.includes(article.article));
+                newArticles.push(...newArticlesFiltered)
 
+                //Convert date field Strings to Timestamps
+                const articlesWithTimestamps = newArticlesFiltered.map(article => {
+                    const dateTimestamp = Timestamp.fromDate(new Date(article.date));
+                    return { ...article, date: dateTimestamp };
+                });
+                
+                //Save new articles in Firestore
+                const categoryRef = collection(db, 'articles', category, 'list');
                 for (const article of articlesWithTimestamps) {
-                    await addDoc(categoryRef, article); // Check this line as well
+                    await addDoc(categoryRef, article);
+                    newArticlesCounter++;
                 }
             }
         }
-
-        res.json(allArticles);
+        console.log("New articles added : ", newArticlesCounter);
+        console.log("New Articles : ",newArticles.map((article) => article.title));
+        console.log("Finished fetchAndProcessArticles function!");
     } catch (error) {
+        console.error(error);
+    }
+}
+
+async function getAllArticles(req, res) {
+    try {
+        await fetchAndProcessArticles();
+    } catch (error) {
+        console.error(error);
         res.status(500).send(error.message);
     }
 }
 
+const articlesAutoFetch = new CronJob('0 * * * * *', fetchAndProcessArticles);
+articlesAutoFetch.start();
 
-async function getEconomyArticles(req, res) {
-    const categories = ['economy']
-    const economyArticles = []
-    for (const website in websites) {
-        for (const category of categories) {
-            const articles = await fetchArticlesFromWebsite(website, category);
-            economyArticles.push(...articles)
-        }
-    }
-    res.json(economyArticles)
-}
-
-async function getPoliticArticles(req, res) {
-    const categories = ['politics']
-    const politicsArticles = []
-    for (const website in websites) {
-        for (const category of categories) {
-            const articles = await fetchArticlesFromWebsite(website, category);
-            politicsArticles.push(...articles)
-        }
-    }
-    res.json(politicsArticles)
-}
-
-module.exports = {getAllArticles, getEconomyArticles, getPoliticArticles}
+module.exports = { getAllArticles };
--- backend/routes/articleRoutes.js
+++ backend/routes/articleRoutes.js
@@ -2,8 +2,5 @@ const express = require('express');
 const router = express.Router();
 const articleController = require('../controllers/articleController')
 
-router.get('/articles/all',articleController.getAllArticles)
-router.get('/articles/economy',articleController.getEconomyArticles)
-router.get('/articles/politics',articleController.getPoliticArticles)
 
 module.exports = router;

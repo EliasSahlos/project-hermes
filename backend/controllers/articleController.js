2429cbd9dcb70fbabb2a211be234740270d11c3b
EliasSahlos <esahlos12@gmail.com>
2024-03-01 11:52:13 +0200

    Added getArticleById Controller and Route && Each article has a new uuid field.

--- backend/controllers/articleController.js
+++ backend/controllers/articleController.js
@@ -1,5 +1,5 @@
 const scraper = require('../scrapper/scrapper')
-const { MongoClient } = require('mongodb')
+const { MongoClient, ObjectId } = require('mongodb')
 const { v4: uuidv4 } = require('uuid')
 require('dotenv').config()
 
@@ -21,10 +21,12 @@ async function scrapeArticles(req, res) {
         let newArticlesArr = []
         console.log('Inserting articles to database...')
         for (const article of articles) {
+
             //Check if article already exists in database
             const existingArticle = await articlesCollection.findOne({ url: article.url })
             if (!existingArticle) {
                 const newArticle = {
+                    uuid: uuidv4(),
                     url: article.url,
                     title: article.title,
                     content: article.content,
@@ -67,4 +69,32 @@ async function getAllArticles(req, res) {
     }
 }
 
-module.exports = { scrapeArticles, getAllArticles }
+async function getArticleById(req, res) {
+    const client = new MongoClient(uri)
+    try {
+        const { id } = req.params
+        console.log('Article ID:', id)
+
+        // Connects to Database
+        await client.connect()
+        const db = client.db('app-data')
+        const articlesCollection = db.collection('articles')
+
+        // Find the article by its ID
+        const article = await articlesCollection.findOne({ uuid: id })
+
+        if (!article) {
+            return res.status(404).json({ message: 'Article not found' })
+        }
+
+        // Return the article
+        res.json(article)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: 'Internal Server Error' })
+    } finally {
+        await client.close()
+    }
+}
+
+module.exports = { scrapeArticles, getAllArticles, getArticleById }

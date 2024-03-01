349cc3ecce13ec09bb1080da918638027fec50b6
EliasSahlos <esahlos12@gmail.com>
2024-03-01 13:18:36 +0200

    FormatDate added as external file. Working on Single Page UI

--- frontend/src/app/feed/[id]/page.tsx
+++ frontend/src/app/feed/[id]/page.tsx
@@ -3,49 +3,66 @@ import axios from "axios";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import Image from "next/image";
+import SpinnerLoading from "@/components/shared/spinner-loading/spinner-loading";
+import QueryBuilderIcon from '@mui/icons-material/QueryBuilder';
+import { formatDate } from "@/app/formatDate";
 
 function ArticleSinglePage() {
     const { id } = useParams();
     const [article, setArticle] = useState<Article>();
+    const [isLoading, setIsLoading] = useState<boolean>(true)
 
     useEffect(() => {
         fetchArticleById(id)
     }, [id]);
 
     async function fetchArticleById(articleId: any) {
+        setIsLoading(true)
         try {
             const response = await axios.get(`http://localhost:3001/api/articles/${articleId}`);
             setArticle(response.data);
+            setIsLoading(false)
         } catch (error) {
             console.error('Error fetching article:', error);
+            setIsLoading(false)
         }
-    }
-
-    function formatDate(isoDate: string, locale: string = "en-US"): string {
-        const date: Date = new Date(isoDate)
-        const options: Intl.DateTimeFormatOptions = { month: "short", day: "numeric", weekday: "long" }
-        return date.toLocaleDateString(locale, options)
     }
 
     return (
         <div className='mt-28 xl:mx-40 p-4'>
-            <div className="flex justify-center items-center">
-                <h1 className="text-4xl font-semibold text-justify">{article?.title}</h1>
-            </div>
-            <div className="flex justify-center items-center mt-10">
-                {article?.image && <Image className="rounded-md " src={article.image} alt="broken-article-img" width={800} height={700} />}
-            </div>
-            <div className="flex flex-row mt-4">
-                <div className="mx-4">
-                    {article?.time && formatDate(article.time)}
-                </div>
-                <div className="mx-4">
-                    {article?.source}
-                </div>
-            </div>
-            <div className="my-10">
-                <p className="text-justify">{article?.content}</p>
-            </div>
+            {isLoading && <SpinnerLoading />}
+            {!isLoading && article &&
+                <div className="bg-white p-4 ">
+                    <div className="flex justify-start mb-4">
+                        <h2 className="text-gray-500 text-sm capitalize">{article?.source}</h2>
+                        <span className="text-purple-500 font-bold mx-1">&gt;</span>
+                        <h2 className="font-semibold text-sm capitalize"> {article?.category}</h2>
+                    </div>
+                    <div className="flex justify-start my-1">
+                        <QueryBuilderIcon fontSize="small" />
+                        <h2 className="text-sm mx-2">{formatDate(article.time)}</h2>
+
+                    </div>
+                    <div className="flex justify-center items-center">
+                        <div>
+                            <h1 className="text-3xl font-semibold text-justify">{article?.title}</h1>
+                        </div>
+                    </div>
+                    <div className="flex justify-center items-center mt-10">
+                        {article?.image && <Image className="" src={article.image} alt="broken-article-img" width={800} height={700} />}
+                    </div>
+                    <div className="flex flex-row mt-4">
+                        <div className="mx-4">
+                            {article?.time && formatDate(article.time)}
+                        </div>
+                        <div className="mx-4">
+                            {article?.source}
+                        </div>
+                    </div>
+                    <div className="my-5">
+                        <p className="text-justify">{article?.content}</p>
+                    </div>
+                </div>}
         </div>
     );
 }
--- frontend/src/components/feed/articles-card/articles-card.tsx
+++ frontend/src/components/feed/articles-card/articles-card.tsx
@@ -2,15 +2,10 @@ import Link from "next/link"
 import BookmarkIcon from '@mui/icons-material/Bookmark';
 import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
 import { usePagination } from "@/context/pagination-context";
+import { formatDate } from "@/app/formatDate";
 
 function ArticlesCard() {
     const { currentPosts } = usePagination()
-
-    function formatDate(isoDate: string, locale: string = "en-US"): string {
-        const date: Date = new Date(isoDate)
-        const options: Intl.DateTimeFormatOptions = { month: "short", day: "numeric", weekday: "long" }
-        return date.toLocaleDateString(locale, options)
-    }
 
     return (
         <div>
@@ -19,21 +14,22 @@ function ArticlesCard() {
                     <div className="grid gap-6 md:grid-cols-2 xl:grid-cols-3">
                         {currentPosts.map((article) => (
                             <div key={article.uuid} className="relative h-[270px] rounded overflow-hidden">
-                                <Link href={`/feed/${article.uuid}`}>
-                                    <img
-                                        src={article.image}
-                                        alt='broken-img'
-                                        className="w-full h-full bg-cover"
-                                    />
-                                    <div className="absolute top-0 left-0 w-full h-[12%] bg-white/30 backdrop-blur-lg z-50">
-                                        <div className="flex justify-between items-center px-2 py-1">
-                                            <h1 className="text-white text-sm">{article.source}</h1>
-                                            <div className="text-white text-sm hover:scale-110 ease-in duration-200 cursor-pointer">
-                                                <BookmarkBorderIcon />
-                                            </div>
-                                        </div>
 
+                                <img
+                                    src={article.image}
+                                    alt='broken-img'
+                                    className="w-full h-full bg-cover"
+                                />
+                                <div className="absolute top-0 left-0 w-full h-[12%] bg-white/30 backdrop-blur-lg z-50">
+                                    <div className="flex justify-between items-center px-2 py-1">
+                                        <h1 className="text-white text-sm">{article.source}</h1>
+                                        <div className="text-white text-sm hover:scale-110 ease-in duration-200 cursor-pointer">
+                                            <BookmarkBorderIcon />
+                                        </div>
                                     </div>
+
+                                </div>
+                                <Link href={`/feed/${article.uuid}`}>
                                     <div
                                         className="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[rgba(0,0,0,0.4)] bg-fixed z-40">
                                         <div className="flex h-full items-end justify-start">

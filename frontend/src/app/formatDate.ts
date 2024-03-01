d4c7b8f4ff0118f9308424e249692aa69aef634e
EliasSahlos <esahlos12@gmail.com>
2024-03-01 13:52:41 +0200

    formatDate shows year as well

--- frontend/src/app/formatDate.ts
+++ frontend/src/app/formatDate.ts
@@ -1,5 +1,5 @@
 export function formatDate(isoDate: string, locale: string = "en-US"): string {
     const date: Date = new Date(isoDate)
-    const options: Intl.DateTimeFormatOptions = { month: "short", day: "numeric", weekday: "long" }
+    const options: Intl.DateTimeFormatOptions = { month: "short", day: "numeric", weekday: "long",year: "numeric" }
     return date.toLocaleDateString(locale, options)
 }
